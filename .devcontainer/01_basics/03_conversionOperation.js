54b1fa0fce201552b08e172922720b6fa9e11f28
yeshwanthpu332-sys <yeshwanthpu332@gmail.com>
2026-04-07 12:22:05 +0000

    operations of datatypes

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -24,7 +24,42 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 28
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
+//************************Operations***************** 
 
+let value = 7
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " yesh"
+
+let str3 = str1+str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+//console.log( (3 + 4) * 5 % 3);
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 +2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
