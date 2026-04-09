91871537f3f85fa7d59662f866b566b11ecea100
yeshwanthpu332-sys <yeshwanthpu332@gmail.com>
2026-04-09 14:19:01 +0000

    math and num

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score = 700
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber = 133.8966
+
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++  Maths  ++++++++++++++++++++++++
+
+//console.log(Math);
+//console.log(Math.abs(-7));
+//console.log(Math.round(4.6));
+//console.log(Math.ceil(8.2));
+//console.log(Math.floor(5.9));
+//console.log(Math.min(6, 9, 2, 7));
+//console.log(Math.max(6, 9, 2, 7));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
